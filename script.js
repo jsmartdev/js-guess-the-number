49e0822051cee6791d448b73a027b4c07ac0e2b1
Jesse Smart <jsmartdev@protonmail.com>
2024-07-26 20:05:15 -0400

    worked on ui elements in game object

--- script.js
+++ script.js
@@ -5,42 +5,115 @@ const gameUIElements = () => {
       children: [
         {
           tagname: 'section',
-          attributes: { id: 'game-start' },
-          content: `<button id="start">Start Game</button>`
+          attributes: { id: 'game-title'},
+          content: `<h1>Guess The Number</h1>`
         },
         {
           tagname: 'section',
-          attributes: { id: 'game-modes' },
+          attributes: { id: 'start-button' },
+          content: `<button id="start" onclick="">Start Game</button>`
+        },
+        {
+          tagname: 'section',
+          attributes: { id: 'modes-buttons' },
           content: `<button id="easy">Easy</button>
                     <button id="normal">Normal</button>
                     <button id="hard">Hard</button>`
         },
-        { 
+        {
           tagname: 'section',
-          attributes: { id: 'random-number' },
-          content: `<h2>${display}</h2>`
+          attributes: { id: 'intro-message' },
+          content: `<h2>You have ${chancesNum} chances to find a number between 1 and ${max}.</h2>
+                    <button id="continue" onclick="">Continue</button>`
+        }, 
+        {
+          tagname: 'section',
+          attributes: { id: 'game-grid' },
+          content: `<div id="minimum" class="large">
+                    </div>
+                    <div id="chances" class="large">
+                    </div>
+                    <div id="guess" class="large">
+                    </div>
+                    <div id="target" class="large">
+                    </div>
+                    <div id="maximum" class="large">
+                    </div>
+                    <div id="turn-1" class="small">
+                    </div>
+                    <div id="turn-2" class="small">
+                    </div>
+                    <div id="turn-3" class="small">
+                    </div>
+                    <div id="turn-4" class="small">
+                    </div>
+                    <div id="turn-5" class="small">
+                    </div>
+                    <div id="turn-6" class="small">
+                    </div>
+                    <div id="turn-7" class="small">
+                    </div>
+                    <div id="turn-8" class="small">
+                    </div>
+                    <div id="turn-9" class="small">
+                    </div>
+                    <div id="turn-10" class="small">
+                    </div>
+                    <div id="turn-11" class="small">
+                    </div>
+                    <div id="turn-12" class="small">
+                    </div>
+                    <div id="turn-13" class="small">
+                    </div>
+                    <div id="turn-14" class="small">
+                    </div>
+                    <div id="turn-15" class="small">
+                    </div>
+                    <div id="turn-16" class="small">
+                    </div>
+                    <div id="turn-17" class="small">
+                    </div>
+                    <div id="turn-18" class="small">
+                    </div>
+                    <div id="turn-19" class="small">
+                    </div>
+                    <div id="turn-20" class="small">
+                    </div>`
         },
         {
           tagname: 'section',
-          attributes: { id: 'player-guess' },
-          content: `<div>
-                      <button id="add-thousand"></button>
-                      <button id="add-hundred"></button>
-                      <button id="add-ten"></button>            
-                      <button id="add-one"></button>
-                    </div>
-                    <div>
-                      <h2 id="thousands-digit">${thousands}</h2>
-                      <h2 id="hundreds-digit">${hundreds}</h2>
-                      <h2 id"tens-digit">${tens}</h2>
-                      <h2 id="ones-digit">${ones}</h2>
-                    </div>
-                    <div>
-                      <button id="minus-thousand"></button>
-                      <button id="minus-hundred"></button>
-                      <button id="minus-ten"></button>            
-                      <button id="minus-one"></button>
-                    </div>`
+          attributes: { id: 'guess-message' },
+          content: `<h2>Type in your number and press Enter.</h2>`
+        },
+        {
+          tagname: 'section',
+          attributes: { id: 'higher-message' },
+          content: `<p>Higher than ${guess}</p>`
+        },
+        {
+          tagname: 'section',
+          attributes: { id: 'lower-message' },
+          content: `<p>Lower than ${guess}</p>`
+        },
+        {
+          tagname: 'section',
+          attributes: { id: 'error-message' },
+          content: `<p>That number is outside the current range of possibilities.</p>`
+        },
+        {
+          tagname: 'section',
+          attributes: { id: 'win-message' },
+          content: `<img src="" alt="thumbs up">
+                    <h2>You win!</h2>
+                    <h2>${target} is the number!</h2>
+                    <h2>Your score is ${score} points!</h2>`
+        },
+        {
+          tagname: 'section',
+          attributes: { id: 'lose-message' },
+          content: `<img src="" alt="thumbs down">
+                    <h2>Game Over! </h2>
+                    <h2>You are out of chances.</h2>`
         }
       ]
     }
