e7c5cb1b83439192f86b2e973fd5e0404dc41931
Jesse Smart <jsmartdev@protonmail.com>
2024-07-29 18:33:36 -0400

    start screen render function

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-
-
-
-const renderStart = () => {
-
+const startScreen = () => {
+  const titleContainer = document.createElement('header');
+  const buttonContainer  = document.createElement('section');
+  const gameTitle = document.createElement('h1');
+  gameTitle.textContent = 'Guess The Secret Number';
+  const easyButton = document.createElement('button');
+  const normalButton = document.createElement('button');
+  const hardButton = document.createElement('button');
+  
+  titleContainer.id = 'title-container';
+  buttonContainer.id = 'button-container';
+  easyButton.id = 'easy-mode';
+  normalButton.id = 'normal-mode';
+  hardButton.id = 'hard-mode';
+  easyButton.textContent = 'Easy';
+  normalButton.textContent = 'Normal';
+  hardButton.textContent = 'Hard';
+  buttonContainer.appendChild(easyButton);
+  buttonContainer.appendChild(normalButton);
+  buttonContainer.appendChild(hardButton);
+  const buttons = buttonContainer.querySelectorAll('button');
+  buttons.forEach(button => button.classList.add('button'));
+  titleContainer.appendChild(gameTitle);
+  return [titleContainer, buttonContainer];
 }
 
-const handleStart = () => {
+const gameScreen = document.getElementById('game-screen');
+const startScreenElements = startScreen();
+startScreenElements.forEach(el => gameScreen.appendChild(el));
+
+const handleModes = () => {
   
 }
 
