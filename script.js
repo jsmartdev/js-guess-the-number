e27eebaf8d55923f9176935d7de5119c43f097b3
Jesse Smart <jsmartdev@protonmail.com>
2024-07-29 19:07:36 -0400

    refactored startScreen function

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const startScreen = () => {
+  
   const titleContainer = document.createElement('header');
   const buttonContainer  = document.createElement('section');
   const gameTitle = document.createElement('h1');
-  gameTitle.textContent = 'Guess The Secret Number';
   const easyButton = document.createElement('button');
   const normalButton = document.createElement('button');
   const hardButton = document.createElement('button');
@@ -12,21 +12,24 @@ const startScreen = () => {
   easyButton.id = 'easy-mode';
   normalButton.id = 'normal-mode';
   hardButton.id = 'hard-mode';
+  gameTitle.textContent = 'Guess The Secret Number';
   easyButton.textContent = 'Easy';
   normalButton.textContent = 'Normal';
   hardButton.textContent = 'Hard';
+  
   buttonContainer.appendChild(easyButton);
   buttonContainer.appendChild(normalButton);
   buttonContainer.appendChild(hardButton);
+  titleContainer.appendChild(gameTitle);
+  
   const buttons = buttonContainer.querySelectorAll('button');
   buttons.forEach(button => button.classList.add('button'));
-  titleContainer.appendChild(gameTitle);
+  
   return [titleContainer, buttonContainer];
+
 }
 
-const gameScreen = document.getElementById('game-screen');
-const startScreenElements = startScreen();
-startScreenElements.forEach(el => gameScreen.appendChild(el));
+startScreen().forEach(el => document.body.appendChild(el));
 
 const handleModes = () => {
   
