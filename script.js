5c9eec9d5d3a2c677250c678dbe5669a31f2ddf1
Jesse Smart <jsmartdev@protonmail.com>
2024-07-30 14:33:09 -0400

    renderStartScreen and handleStartButton functions operational

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 const makeElement = (tagName, id = '', text = '', classNames = []) => {
   const element = document.createElement(tagName);
   if (id) {
@@ -32,30 +33,54 @@ const clearChildren = (parent) => {
   }
 };
 
-const startScreen = () => {
-  const titleContainer = makeElement('header', 'title-container');
-  const buttonContainer  = makeElement('section', 'button-container');
-  const gameTitle = makeElement('h1', '', 'Guess The Secret Number');
-  const easyButton = makeElement('button', 'easy-mode', 'Easy', 'button-class');
-  const normalButton = makeElement('button', 'normal-mode', 'Normal', 'button-class');
-  const hardButton = makeElement('button', 'hard-mode', 'Hard', 'button-class');
-  
-  buttonContainer.appendChild(easyButton);
-  buttonContainer.appendChild(normalButton);
-  buttonContainer.appendChild(hardButton);
-  titleContainer.appendChild(gameTitle);
-  
-  return [titleContainer, buttonContainer];
-}
-
-startScreen().forEach(el => document.body.appendChild(el));
-
-const handleModes = () => {
-  
-}
-
 const displayNumber = () => {
   let hiddenInput = document.getElementById('number-input');
   let guessField = document.getElementById('guess-container');
   guessField.textContent = hiddenInput.value;
 }
+
+const startScreen = () => {
+  const titleContainer = makeElement('header', 'title-container');
+  const buttonContainer  = makeElement('section', 'button-container');
+  const gameTitle = makeElement('h1', '', 'Guess The Secret Number');
+  const startButton = makeElement('button', 'start-game', 'Start Game', ['button'])
+  startButton.addEventListener('click', handleStartButton);
+  buttonContainer.appendChild(startButton);
+  titleContainer.appendChild(gameTitle);
+  
+  return [titleContainer, buttonContainer];
+}
+
+const handleStartButton = () => {
+  const buttonContainer = document.getElementById('button-container');
+  clearChildren(buttonContainer);
+  const easyButton = makeElement('button', 'easy-mode', 'Easy', ['button'] );
+  const normalButton = makeElement('button', 'normal-mode', 'Normal', ['button'] );
+  const hardButton = makeElement('button', 'hard-mode', 'Hard', ['button'] );
+  
+  
+  
+  buttonContainer.appendChild(easyButton);
+  buttonContainer.appendChild(normalButton);
+  buttonContainer.appendChild(hardButton);
+}
+
+
+
+const gameField = document.getElementById('game-field');
+const startGame = document.getElementById('start-game');
+
+
+const renderStartScreen = () => {
+  startScreen().forEach(el => gameField.appendChild(el));
+  
+}
+
+renderStartScreen();
+
+
+
+
+
+
+
